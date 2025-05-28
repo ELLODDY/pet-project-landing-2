bd303da3a370c7ae5b01f7628243909a93917cac
Анна Кутузова <anna@MacBook-Air-Anna.local>
2025-05-28 17:47:49 +0300

    Добавлены ремонтируемые бренды и реализована функциональность скрытия последних трех брендов в листе

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-// не подключен
 
+const hide__button = document.querySelector('.hide__button');
+const hidden__brand__buttons = document.querySelector('.hidden');
+const showAll__button = document.querySelector('.showAll__button__hidden');
+
+hide__button.addEventListener('click', () => {
+    hide__button.classList.toggle('hideButtonHidden');
+    hidden__brand__buttons.classList.toggle('statusHidden');
+    showAll__button.classList.toggle('showAll__button__visible')
+});
+
+showAll__button.addEventListener('click', () => {
+    showAll__button.classList.remove('showAll__button__visible');
+    hide__button.classList.remove('hideButtonHidden');
+    hidden__brand__buttons.classList.remove('statusHidden');
+})
