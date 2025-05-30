dcf9ae4efdbd3866356df0ba4ae8cf2996636639
Анна Кутузова <anna@MacBook-Air-Anna.local>
2025-05-30 19:05:52 +0300

    Рефакторинг кода

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const showAll__button = document.querySelector('.showAll__button__hidden');
 
 hide__button.addEventListener('click', () => {
     hide__button.classList.toggle('hideButtonHidden');
-    hidden__brand__buttons.classList.toggle('statusHidden');
-    showAll__button.classList.toggle('showAll__button__visible')
+    hidden__brand__buttons.classList.toggle('brandsHiddenString');
+    showAll__button.classList.toggle('showAll__button__visible');
 });
 
 showAll__button.addEventListener('click', () => {
     showAll__button.classList.remove('showAll__button__visible');
     hide__button.classList.remove('hideButtonHidden');
-    hidden__brand__buttons.classList.remove('statusHidden');
+    hidden__brand__buttons.classList.remove('brandsHiddenString');
 })
