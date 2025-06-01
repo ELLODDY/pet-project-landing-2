a4dd4b22e903c4620ec46e855a154432d4cb8be0
Анна Кутузова <anna@MacBook-Air-Anna.local>
2025-06-01 18:35:08 +0300

    Адаптация макета под три устройства: десктоп, планшет и телефон

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 
 const hide__button = document.querySelector('.hide__button');
-const hidden__brand__buttons = document.querySelector('.hidden');
 const showAll__button = document.querySelector('.showAll__button__hidden');
 
 hide__button.addEventListener('click', () => {
     hide__button.classList.toggle('hideButtonHidden');
-    hidden__brand__buttons.classList.toggle('brandsHiddenString');
+    hiddenBransCard();
     showAll__button.classList.toggle('showAll__button__visible');
 });
+
+function hiddenBransCard() {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    for (let i = 0; i < hiddenElements.length; i++) {
+        const element = hiddenElements[i];
+        element.classList.toggle('brandsHiddenString');
+    };
+};
 
 showAll__button.addEventListener('click', () => {
     showAll__button.classList.remove('showAll__button__visible');
     hide__button.classList.remove('hideButtonHidden');
-    hidden__brand__buttons.classList.remove('brandsHiddenString');
+    hiddenBransCard();
+});
+
+const mainTextHidden = document.querySelector('.main__text--hidden');
+const readFurther = document.querySelector('.readFurther__button--visible');
+const mainTextChange = document.querySelector('.main__text__change');
+const textHideButton = document.querySelector('.textHide__button--hidden');
+
+readFurther.addEventListener('click', () => {
+    readFurther.classList.toggle('readFurther__button--hidden');
+    mainTextChange.textContent = ('Мы успешно работаем с 1992 года и заслужили репутацию надежного партнера, что подтверждает большое количество постоянных клиентов.');
+    mainTextHidden.classList.toggle('main__text--visible');
+    textHideButton.classList.toggle('textHide__button--visible');
+});
+
+textHideButton.addEventListener('click', () => {
+    textHideButton.classList.remove('textHide__button--visible');
+    mainTextChange.textContent = ('Мы успешно работаем с 1992 года и заслужили репутацию надежного партнера, что подтверждает большое количество постоянных клиентов...');
+    mainTextHidden.classList.remove('main__text--visible');
+    readFurther.classList.remove('readFurther__button--hidden');
 })
